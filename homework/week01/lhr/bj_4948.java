f887f1febe3fbdf9824cf67e9dcb3c7d764f6c0c
nhyxx <leehyeoilin@gmail.com>
2025-03-19 16:00:44 +0900

    code update

--- homework/week01/lhr/bj_4948.java
+++ homework/week01/lhr/bj_4948.java
@@ -3,9 +3,10 @@ import java.util.*;
 public class bj_4948 {
 	public static boolean[] prime;
     
+	// 소수 찾는 메소드 - 에라토스테네스의 체
     public static void IsPrime(int n) {
     	prime = new boolean[2 * n + 1];
-    	prime[0] = true;
+    	prime[0] = true; // 0, 1은 소수가 아님
     	prime[1] = true;
     	
     	for (int i = 2; i <= Math.sqrt(2 * n); i++) {
